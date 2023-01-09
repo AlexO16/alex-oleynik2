0860f06895498951f71cc6e11b96874930720d6e
Alex Oleynik <alexoleynik74@gmail.com>
2023-01-09 15:27:00 -0600

    styling

--- src/components/cards/ContactCard.jsx
+++ src/components/cards/ContactCard.jsx
@@ -2,14 +2,14 @@ import { Card } from "react-bootstrap";
 
 function ContactCard({ item }) {
     return (
-        <a className="col-md" href={item.url} > 
-    <Card className="m-4 text-black border-0" style={{ vw: "100%", background: "whitesmoke" }}>
-      {item.icon}
-      <Card.Body>
-                <Card.Title className="platName" style={{ textAlign: "center" }}>{item.platformName}</Card.Title>
-            </Card.Body>
-    </Card>
-    </a>
+        <a className="col-md" href={item.url} >
+            <Card className="m-4 text-black border-0" style={{ vw: "100%", background: "whitesmoke" }}>
+                {item.icon}
+                <Card.Body>
+                    <Card.Title className="platName" style={{ textAlign: "center" }}>{item.platformName}</Card.Title>
+                </Card.Body>
+            </Card>
+        </a>
     )
 };
 
